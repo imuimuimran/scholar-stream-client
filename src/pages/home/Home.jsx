47b670a61303a60c78a15a1dc61bd2f224cbb4e6
brainbeex <brainbeex@gmail.com>
2026-06-10 22:10:39 +0600

    grid issue solved

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -172,7 +172,7 @@ const Home = () => {
 
         </div>
 
-        <div className="grid md:grid-cols-2 lg:grid-cols-2 gap-8">
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
 
           {scholarships.map((scholarship) => (
 
